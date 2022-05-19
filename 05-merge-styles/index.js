3054af0e0789c7d96616af38993fe9c8f5ef8f84
Tabunov Egor <Vonubat@gmail.com>
2022-05-19 18:33:05 +0300

    feat: add solution for 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const stylesFolder = path.join(__dirname, './styles');
+const projectDistFolder = path.join(__dirname, './project-dist');
+const bundleCss = fs.createWriteStream(
+  path.join(projectDistFolder, 'bundle.css')
+);
+
+async function mergeStyles(origin, dest) {
+  try {
+    const files = await fs.promises.readdir(origin, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        const read = fs.createReadStream(path.join(origin, file.name), 'utf-8');
+        read.pipe(dest);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+mergeStyles(stylesFolder, bundleCss);
